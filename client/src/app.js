653f233dbec69342f9edaaf7126dfa5058928649
yonatan786 <63449154+yonatan786@users.noreply.github.com>
2025-02-23 18:14:12 +0200

    Create app.js

---
+++ client/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HebrewCalendar from './components/HebrewCalendar';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <HebrewCalendar />
+    </div>
+  );
+}
+
+export default App;
